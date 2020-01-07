7745fa05de64d430b457f3bbb376ff8606616998
shawon1fb <shawon0187@gmail.com>
2020-01-07 23:31:39 +0600

    first commit

---
+++ app/src/main/java/com/shahanulshaheb/android_gps_service/GPS_Service.java
@@ -0,0 +1,72 @@
+package com.shahanulshaheb.android_gps_service;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class GPS_Service extends Service {
+
+    private LocationListener listener;
+    private LocationManager locationManager;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onCreate() {
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                System.out.println("==============location=======================");
+                Log.i("location ",location.toString());
+                System.out.println(location.toString());
+                Intent i = new Intent("location_update");
+                i.putExtra("coordinates", location.getLongitude() + " " + location.getLatitude());
+                sendBroadcast(i);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        };
+
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, listener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            locationManager.removeUpdates(listener);
+        }
+    }
+}
---
+++ app/src/main/java/com/shahanulshaheb/android_gps_service/MainActivity.java
@@ -0,0 +1,110 @@
+package com.shahanulshaheb.android_gps_service;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_start, btn_stop;
+    private TextView textView;
+    private BroadcastReceiver broadcastReceiver;
+    int cnt=0;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    cnt++;
+
+                    textView.append("count: "+cnt+"\n" +intent.getExtras().get("coordinates")+"\n");
+
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(broadcastReceiver != null){
+            unregisterReceiver(broadcastReceiver);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_start = (Button) findViewById(R.id.button);
+        btn_stop = (Button) findViewById(R.id.button2);
+        textView = (TextView) findViewById(R.id.textView);
+
+        if(!runtime_permissions())
+            enable_buttons();
+
+    }
+
+
+    private boolean runtime_permissions() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                enable_buttons();
+            } else {
+                runtime_permissions();
+            }
+        }
+    }
+
+
+    private void enable_buttons() {
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), GPS_Service.class);
+                startService(i);
+            }
+        });
+
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(), GPS_Service.class);
+                stopService(i);
+
+            }
+        });
+
+    }
+}
